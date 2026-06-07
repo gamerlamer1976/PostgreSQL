384da9e73ec94926d61240405beac0a3f51de24e
Gamer Lamer <ntyntyytf@gmail.com>
2026-06-08 00:06:06 +0700

    Добавлена исправленная версия скрипта 2.0

---
+++ create_tables_fix2.sql
@@ -0,0 +1,58 @@
+CREATE TABLE genres (
+    genre_id SERIAL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (genre_id)
+);
+
+CREATE TABLE artists (
+    artist_id SERIAL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (artist_id)
+);
+
+CREATE TABLE albums (
+    album_id SERIAL,
+    name VARCHAR(255) NOT NULL,
+    release_year INTEGER NOT NULL,
+    PRIMARY KEY (album_id)
+);
+
+CREATE TABLE compilations (
+    compilation_id SERIAL,
+    name VARCHAR(255) NOT NULL,
+    release_year INTEGER NOT NULL,
+    PRIMARY KEY (compilation_id)
+);
+
+CREATE TABLE tracks (
+    track_id SERIAL,
+    name VARCHAR(255) NOT NULL,
+    duration_seconds INTEGER NOT NULL,
+    album_id INTEGER,
+    PRIMARY KEY (track_id),
+    FOREIGN KEY (album_id) REFERENCES albums(album_id)
+);
+
+CREATE TABLE artist_genres (
+    artist_id INTEGER NOT NULL,
+    genre_id INTEGER NOT NULL,
+    PRIMARY KEY (artist_id, genre_id),
+    FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
+    FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
+);
+
+CREATE TABLE album_artists (
+    album_id INTEGER NOT NULL,
+    artist_id INTEGER NOT NULL,
+    PRIMARY KEY (album_id, artist_id),
+    FOREIGN KEY (album_id) REFERENCES albums(album_id),
+    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+);
+
+CREATE TABLE compilation_tracks (
+    compilation_id INTEGER NOT NULL,
+    track_id INTEGER NOT NULL,
+    PRIMARY KEY (compilation_id, track_id),
+    FOREIGN KEY (compilation_id) REFERENCES compilations(compilation_id),
+    FOREIGN KEY (track_id) REFERENCES tracks(track_id)
+);
